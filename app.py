2e251859bfba8842a0394aaf5db7c4eae0028de9
Hafsa-Ahmed905 <bsef23a007@pucit.edu.pk>
2025-11-16 02:24:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
 print("Hello, Jenkins!")
-unused_variable = 123     
-x = "1" + 2              
+
+unused_variable = 123    # Pylint should warn
+x = "1" + 2              # Mypy type warning
