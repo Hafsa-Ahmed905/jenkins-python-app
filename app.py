157ae0c0e38dd12c169b96fbb177ec60cb3c97ab
Hafsa-Ahmed905 <bsef23a007@pucit.edu.pk>
2025-11-16 02:33:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # app.py
+
 print("Hello, Jenkins!")
 
-unused_variable = 123    # Pylint should warn
-x = "1" + 2              # Mypy type warning
+unused_variable = 123        # Pylint warning: unused variable
+x = "1" + 2                  # Mypy type warning: incompatible types
+def f():
+    pass                      # Flake8 may complain if style rules broken
